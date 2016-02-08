e72f66efd53c81784e84af26d5dfb2b99991bffa
Sofia Celi <sceli@thoughtworks.com>
2016-02-08 12:48:27 -0500

    New scene

---
+++ src/Mordor.java
@@ -0,0 +1,18 @@
+/**
+ * Created by sceli on 2/8/16.
+ */
+
+public class Mordor {
+
+    public void printStory() {
+        System.out.println("Sauron, the Dark Lord of Mordor, has been searching for the One Ring for 20 years. \n" +
+                "He has finally captured Frodo, from the Shire, heir of the Ring, but, still, he can't find the One Ring. \n" +
+                "In his possesion, he has the bag of Frodo. But he only has one chance to grab an item out of it. \n" +
+                "Should be or should not be the ring, will determine his fate...\n" +
+                "===================================================== \n" +
+                "Your name is Frodo, you are a hobbit and you are in Mordor. Sauron has your bag in his possesion \n" +
+                "He has one chance. If he grabs the One Ring from the bag, you're doomed \n" +
+                "but if not, Sauron will dissapear and you'll win. \n" +
+                "Now, let's wait: Sauron is grabbing something from the bag.");
+    }
+}
